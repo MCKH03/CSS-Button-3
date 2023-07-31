f70c2118995c499809396541be2cda3d5b2e8799
Mehrshad Cheshm Khavari <ytmehrshad@gmail.com>
2023-07-31 17:54:34 +0330

    Add some animation to the project

--- script.js
+++ script.js
@@ -6,20 +6,31 @@ const wait = document.querySelector(".wait");
 const before = document.querySelector(".before");
 const complete = document.querySelector(".complete");
 const text = document.querySelector(".text");
+const ball = document.querySelectorAll(".ball");
 
+let working = false;
 container.addEventListener("click", function () {
-  container.classList.add("width");
-  icon.classList.add("opacity-0");
-  wait.classList.add("opacity-1");
-  before.classList.add("width-before");
-  setTimeout(() => {
-    wait.classList.remove("opacity-1");
-    before.classList.add("opacity-0");
-  }, 2500);
-  complete.classList.add("opacity-1");
-  setTimeout(() => {
-    complete.classList.remove("opacity-1");
-    complete.style.transition = "all 0.4s 1s";
-  }, 5000);
-  text.classList.add("opacity-1");
+  if (working === false) {
+    working = true;
+    container.classList.add("width");
+    icon.classList.add("opacity-0");
+    wait.classList.add("opacity-1");
+    before.classList.add("width-before");
+    setTimeout(() => {
+      wait.classList.remove("opacity-1");
+      before.classList.add("opacity-0");
+    }, 2500);
+    setTimeout(() => {
+      ball.forEach((ball) => {
+        ball.classList.remove("opacity-0");
+        ball.classList.add("opacity-1");
+      });
+    }, 3800);
+    complete.classList.add("opacity-1");
+    setTimeout(() => {
+      complete.classList.remove("opacity-1");
+      complete.style.transition = "all 0.4s 1s";
+    }, 5000);
+    text.classList.add("opacity-1");
+  }
 });
